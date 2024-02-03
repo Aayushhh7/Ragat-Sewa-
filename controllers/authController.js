7811ab59df2963665414a921c81f2df5a524dbb3
Ayush <ayushkhatiwada420@gmail.com>
2024-02-03 15:02:46 +0545

    Create login API using JWT and Postman

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { use } = require("../routes/authRoutes");
 
 const registerController = async (req, res) => {
   try {
@@ -34,4 +36,43 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = { registerController };
+//Login Call Back
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+    //Check and Compare Password
+    const comparePassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!comparePassword) {
+      return res.status(500).send({
+        sucess: false,
+        message: "Invalid Credentials",
+      });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res.status(200).send({
+      sucess: true,
+      message: "Login Succesfully",
+      token,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Login API Error ",
+    });
+  }
+};
+
+module.exports = { registerController, loginController };
